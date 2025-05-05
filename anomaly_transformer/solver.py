bef8fb70283bc95cd8bd242e9d99674ada4c5c3c
JadeLee274 <tmdals274@snu.ac.kr>
2025-05-05 20:33:50 +0900

    anotran solver

--- anomaly_transformer/solver.py
+++ anomaly_transformer/solver.py
@@ -5,6 +5,8 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from sklearn.metrics import precision_recall_fscore_support as prf
+from sklearn.metrics import accuracy_score
 from utils.utils import *
 from model.transformer import AnomalyTransformer
 from data_factory.data_loader import get_loader_segment
@@ -167,8 +169,8 @@ class Solver(object):
                     torch.mean(
                         my_kl_loss(
                             series[u],
-                            prior[u] / torch.unsqueeze(torch.sum(prior[u], dim=-1), dim=-1).repeat(1, 1, 1, self.window_size)
-                        ).detach()
+                            (prior[u] / torch.unsqueeze(torch.sum(prior[u], dim=-1), dim=-1).repeat(1, 1, 1, self.window_size)).detach()
+                        )
                     )
                     + torch.mean(
                         my_kl_loss(
@@ -323,7 +325,7 @@ class Solver(object):
 
         # (1) statistic on the train set
         attens_energy = []
-        for i, (input_data, labels) in enumerate(self.train_loader):
+        for _, (input_data, _) in enumerate(self.train_loader):
             input = input_data.float().to(self.device)
             output, series, prior, _ = self.model(input)
             loss = torch.mean(criterion(input, output), dim=-1)
@@ -360,5 +362,129 @@ class Solver(object):
         train_energy = np.array(attens_energy)
 
         # (2) find the threshold
-        
+        attens_energy = []
+        for i, (input_data, labels) in enumerate(self.thre_loader):
+            input = input_data.float().to(self.device)
+            output, series, prior, _ = self.model(input)
+            loss = torch.mean(criterion(input, output), dim=-1)
+            series_loss = 0.0
+            prior_loss = 0.0
 
+            for u in range(len(prior)):
+                if u == 0:
+                    series_loss = my_kl_loss(
+                        series[u],
+                        (prior[u] / torch.unsqueeze(torch.sum(prior[u], dim=-1), dim=-1).repeat(1, 1, 1, self.window_size)).detach()
+                    ) * temperature
+
+                    prior_loss = my_kl_loss(
+                        (prior[u] / torch.unsqueeze(torch.sum(prior[u], dim=-1), dim=-1).repeat(1, 1, 1, self.window_size)),
+                        series[u].detach()
+                    ) * temperature
+                else:
+                    series_loss += my_kl_loss(
+                        (prior[u] / torch.unsqueeze(torch.sum(prior[u], dim=-1), dim=-1).repeat(1, 1, 1, self.window_size)),
+                        series[u].detach()
+                    ) * temperature
+
+                    prior_loss += my_kl_loss(
+                        (prior[u] / torch.unsqueeze(torch.sum(prior[u], dim=-1), dim=-1).repeat(1, 1, 1, self.window_size)),
+                        series[u].detach()
+                    ) * temperature
+            
+            metric = torch.softmax((-series_loss - prior_loss), dim=-1)
+            cri = metric * loss
+            cri = cri.detacu().cpu().numpy()
+            attens_energy.append(cri)
+
+        attens_energy = np.concatenate(attens_energy, axis=0).reshape(-1)
+        test_energy = np.array(attens_energy)
+        combined_energy = np.concatenate([train_energy, test_energy], axis=0)
+        thresh = np.percentile(combined_energy, 100 - self.anomaly_ratio)
+        print("Threshold:", thresh)
+
+        # (3) evaluation on the test set
+        test_labels = []
+        attens_energy = []
+
+        for _, (input_data, _) in enumerate(self.thre_loader):
+            input = input_data.float().to(self.device)
+            output, series, prior, _ = self.model(input)
+            loss = torch.mean(criterion(input, output))
+            series_loss = 0.0
+            prior_loss = 0.0
+
+            for u in range(len(prior)):
+                if u == 0:
+                    series_loss = my_kl_loss(
+                        series[u],
+                        (prior[u] / torch.unsqueeze(torch.sum(prior[u], dim=-1), dim=-1).repeat(1, 1, 1, self.window_size)).detach()
+                    ) * temperature
+
+                    prior_loss = my_kl_loss(
+                        (prior[u] / torch.unsqueeze(torch.sum(prior[u], dim=-1), dim=-1).repeat(1, 1, 1, self.window_size)),
+                        series[u].detach()
+                    ) * temperature
+                else:
+                    series_loss += my_kl_loss(
+                        (prior[u] / torch.unsqueeze(torch.sum(prior[u], dim=-1), dim=-1).repeat(1, 1, 1, self.window_size)),
+                        series[u].detach()
+                    ) * temperature
+
+                    prior_loss += my_kl_loss(
+                        (prior[u] / torch.unsqueeze(torch.sum(prior[u], dim=-1), dim=-1).repeat(1, 1, 1, self.window_size)),
+                        series[u].detach()
+                    ) * temperature
+
+            cri = metric * loss
+            cri = cri.detach().cpu().numpy()
+            attens_energy.append(cri)
+            test_labels.append(labels)
+
+        attens_energy = np.concatenate(attens_energy, axis=0).reshape(-1)
+        test_labels = np.concatenate(test_labels, axis=0).reshape(-1)
+        test_energy = np.array(attens_energy)
+        test_labels = np.array(test_labels)
+        pred = (test_energy > thresh).astype(int)
+        gt = test_labels.astype(int)
+        print("pred:", pred.shape)
+        print('gt:', gt.shape)
+
+        # detection adjustment
+        anomaly_state = False
+        for i in range(len(gt)):
+            if gt[i] == 1 and pred[i] == 1 and not anomaly_state:
+                anomaly_state = True
+                for j in range(i, 0, -1):
+                    if gt[j] == 0:
+                        break
+                    else:
+                        if pred[j] == 0:
+                            pred[j] = 1
+                for j in range(i, len(gt)):
+                    if gt[j] == 0:
+                        break
+                    else:
+                        if pred[j] == 0:
+                            pred[j] = 1
+            elif gt[i] == 0:
+                anomaly_state = False
+            if anomaly_state:
+                pred[i] = 1
+
+            pred = np.array(pred)
+            gt = np.array(gt)
+            print('pred:', pred.shape)
+            print('gt:', gt.shape)
+            
+            accuracy = accuracy_score(gt, pred)
+            precision, recall, f_score, support = prf(
+                gt, pred, average='binary',
+            )
+
+            print(
+                f"Accuracy: {accuracy:.4f} | Precision: {precision:.4f}"
+                + f" | Recall: {recall:.4f} | F-score: {f_score:.4f}"
+            )
+
+            return accuracy, precision, recall, f_score
