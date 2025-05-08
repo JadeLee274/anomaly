3685d7ca1bd42614cbf8c19e560341ff5199717a
JadeLee274 <tmdals274@snu.ac.kr>
2025-05-08 11:08:46 +0900

    fix export default

--- anomaly_transformer/scripts/PSM.sh
+++ anomaly_transformer/scripts/PSM.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=3
 
-python main.py --anomaly_ratio 1 --num_epochs 3 --batch_size 256 --mode train --dataset PSM --data_path ../../data/PSM --input_c 25 --output_c 25
-python main.py --anomaly_ratio 1 --num_epochs 10 --batch_size 256 --mode test --dataset PSM --data_path ../../data/PSM --input_c 25 --output_c 25
+python main.py --anomaly_ratio 1 --num_epochs 3 --batch_size 256 --mode train --dataset PSM --data_path /data/seungmin/PSM --input_c 25 --output_c 25
+python main.py --anomaly_ratio 1 --num_epochs 10 --batch_size 256 --mode test --dataset PSM --data_path /data/seungmin/PSM --input_c 25 --output_c 25
