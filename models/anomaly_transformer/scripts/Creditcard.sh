c042dd51870d7d3494ce974583bcbaf96e83e4c6
JadeLee274 <tmdals274@snu.ac.kr>
2025-07-03 17:04:26 +0900

    adjust anomaly ratio exactly the same as that of data

--- models/anomaly_transformer/scripts/Creditcard.sh
+++ models/anomaly_transformer/scripts/Creditcard.sh
@@ -1,4 +1,4 @@
 export CUDA_VISIBLE_DEVICES=0
 
-python main.py --anomaly_ratio 0.02 --num_epochs 3 --batch_size 256 --mode train --dataset Credit --data_path /data/seungmin/Creditcard --input_c 29 --output_c 29
-python main.py --anomaly_ratio 0.02 --num_epochs 10 --batch_size 256 --mode test --dataset Credit --data_path /data/seungmin/Creditcard --input_c 29 --output_c 29
+python main.py --anomaly_ratio 0.2 --num_epochs 3 --batch_size 256 --mode train --dataset Credit --data_path /data/seungmin/Creditcard --input_c 29 --output_c 29
+python main.py --anomaly_ratio 0.2 --num_epochs 10 --batch_size 256 --mode test --dataset Credit --data_path /data/seungmin/Creditcard --input_c 29 --output_c 29
