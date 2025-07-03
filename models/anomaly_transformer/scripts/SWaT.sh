a09a90b22966c576541d0cee4120177ee368c9ec
JadeLee274 <tmdals274@snu.ac.kr>
2025-07-03 21:52:37 +0900

    add SWaT scripts

---
+++ models/anomaly_transformer/scripts/SWaT.sh
@@ -0,0 +1,4 @@
+export CUDA_VISIBLE_DEVICES=0
+
+python main.py --anomaly_ratio 0.1 --num_epochs 3 --batch_size 256 --mode train --dataset SWaT --data_path /data/seungmin/SWaT --input_c 51 --output_c 51
+python main.py --anomaly_ratio 0.1 --num_epochs 10 --batch_size 256 --mode test --dataset SWaT --data_path /data/seungmin/SWaT --input_c 51 --output_c 51
